e36be90a057c220fe5514cdf8121b4c4ea23793d
khanh gia <luonggiakhanh1997@gmail.com>
2025-07-19 12:07:16 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://contact-api.onrender.com/api", // Thay bằng link Render sau khi deploy
+  baseURL: "https://client-ybb6.onrender.com/api/contacts", // Thay bằng link Render sau khi deploy
 });
 
 export default API;
