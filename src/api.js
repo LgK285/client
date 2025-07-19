13bf171b2a4732131ea2ad2833483c4c54a46b68
khanh gia <luonggiakhanh1997@gmail.com>
2025-07-19 12:03:27 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/contacts", // Thay bằng link Render sau khi deploy
+  baseURL: "https://contact-api.onrender.com/api", // Thay bằng link Render sau khi deploy
 });
 
 export default API;
